1c449fa958b33dff36aa95a5c0d04b9b0be5e2d9
wolfgunblood <jalajdorai1@gmail.com>
2023-05-15 12:58:04 +0530

    Initialized Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React, { useState } from 'react'
 import MenuBar from './components/MenuBar/MenuBar'
+import './App.scss'
+import Sidebar from './components/SideBar/Sidebar'
+import Files from './components/Files/Files'
+import Statusbar from './components/Statusbar/Statusbar'
 
 const App = () => {
   return (
-    <div id='finder'>
-      <MenuBar />
+    <div className='window-container' id='window'>
+      <div className='finder-container' id='finder'>
+        <MenuBar />
+        <div className='main-container' id='main'>
+          <Sidebar />
+          <Files />
+
+        </div>
+        <Statusbar />
+      </div>
     </div>
   )
 }
---
+++ src/components/Statusbar/Statusbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Statusbar = () => {
+  return (
+    <div>Statusbar</div>
+  )
+}
+
+export default Statusbar
